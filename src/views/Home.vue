06922bd5a4161861f41b8ed059510170616cb30d
patrykuran <trykpa@gmail.com>
2020-12-26 14:20:31 +0100

    fetch data from local server

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts"/>
+    </div>
+    <div v-else>Loading...</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import {ref} from 'vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  // component imports
+  import PostList from '../components/PostList.vue'
+
+  export default {
+    name: 'Home',
+    components: {PostList},
+    setup() {
+      const posts = ref([]);
+      const error = ref(null);
+
+      const load = async () => {
+        try {
+          const data = await fetch('http://localhost:3000/posts')
+          if (!data.ok) {
+            throw Error('data is not available')
+          }
+          posts.value = await data.json();
+        } catch
+          (err) {
+          error.value = err.message
+        }
+      }
+
+      load();
+
+      return {posts, error}
+    }
   }
-}
 </script>
